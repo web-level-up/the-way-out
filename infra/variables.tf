b8676602623294871557e1f9d82ac802a279dfa4
Mukhunyeledzi-Muthuphei <Mukhunyeledzi.Muthuphei@bbd.co.za>
2025-05-07 19:30:39 +0200

    RDS instance setup

---
+++ infra/variables.tf
@@ -0,0 +1,11 @@
+variable "db_username" {
+  description = "The username for the database instance"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_password" {
+  description = "The password for the database instance"
+  type        = string
+  sensitive   = true
+}
