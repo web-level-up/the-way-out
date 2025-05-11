f9e8fb47ea3cb2c6c440fa2d76524bdabf4f0a8d
Temwa-BBD <temwa.nyirenda@vcontractor.co.za>
2025-05-11 17:09:36 +0200

    Fixed css for maze selection

--- frontend/scripts/rendering/helpers/helper-functions.js
+++ frontend/scripts/rendering/helpers/helper-functions.js
@@ -3,7 +3,7 @@ import {renderMazeSelectionPage} from "../render-maze-selection-page.js";
 
 export function getRenderBaseGamePagePromise() {
     return loadPage("views/base-game-page.html").then(() => {
-        addNavigationListeners();
+        addSidebarListeners();
     });
 }
 
@@ -33,11 +33,34 @@ export function resetGamePageView() {
     baseGamePage.style.gridTemplateColumns = "0 5vw 95vw";
 }
 
-function addNavigationListeners() {
+function addSidebarListeners() {
+    const sidebarButton = document.getElementById("nav-button");
     const mazeSelectionPageLink = document.getElementById("maze-selection-page-link");
 
     mazeSelectionPageLink.addEventListener("click", () => {
         resetGamePageView();
         renderMazeSelectionPage();
     });
+
+    sidebarButton.addEventListener("click", () => {
+        clickSidebarButton();
+    });
+}
+
+export function clickSidebarButton() {
+    let baseGamePage = document.getElementById("base-game-page");
+    let sidebar = document.getElementById("sidebar");
+    let navButtonImg = document.getElementById("nav-button-img");
+
+    if (sidebar.style.width === "20vw") {
+        sidebar.style.width = "0";
+        navButtonImg.style.transform = "none";
+        baseGamePage.style.gridTemplateAreas = '"sidebar nav-button content"';
+        baseGamePage.style.gridTemplateColumns = "0 5vw 95vw";
+    } else {
+        sidebar.style.width = "20vw";
+        navButtonImg.style.transform = "rotate(180deg)";
+        baseGamePage.style.gridTemplateColumns = "20vw 5vw 75vw";
+        baseGamePage.style.gridTemplateAreas = '"sidebar nav-button content"';
+    }
 }
--- frontend/scripts/rendering/render-maze-card.js
+++ frontend/scripts/rendering/render-maze-card.js
@@ -1,31 +1,31 @@
 import {difficultyLevels} from "../dummy-data.js";
 
 function getMazeSize(mazeLayout) {
-    const layoutLength = mazeLayout.length;
-    const size = Math.sqrt(layoutLength);
+  const layoutLength = mazeLayout.length;
+  const size = Math.sqrt(layoutLength);
 
-    if (Number.isInteger(size)) {
-        return size;
-    } else {
-        return NaN;
-    }
+  if (Number.isInteger(size)) {
+    return `${size} x ${size}`;
+  } else {
+    return NaN;
+  }
 }
 
 export function renderMazeCard(maze) {
-    return fetch(`views/maze-card.html`)
-        .then(response => response.text())
-        .then(content => {
-            const cardContainer = document.createElement("section");
+  return fetch(`views/maze-card.html`)
+    .then(response => response.text())
+    .then(content => {
+      const cardContainer = document.createElement("section");
 
-            cardContainer.innerHTML = content;
+      cardContainer.classList.add("maze-card-container");
+      cardContainer.innerHTML = content;
 
-            cardContainer.querySelector("#maze-title").textContent = `Maze ${maze.id}`;
-            cardContainer.querySelector("#maze-size").textContent = `Size: ${getMazeSize(maze.maze_layout)}`;
-            cardContainer.querySelector("#maze-difficulty").textContent = `Difficulty: ${difficultyLevels.find(level => level.id === maze.difficulty_level_id)?.difficulty_level_name || "Unknown"}`;
-            cardContainer.querySelector("#maze-time-record").textContent = `Time Record: ${"N/A"}`;
-            cardContainer.querySelector("#maze-step-record").textContent = `Step Record: ${"N/A"}`;
-            cardContainer.querySelector("#maze-times-played").textContent = `Times Played By Users: ${"N/A"}`;
+      cardContainer.querySelector("#maze-card-title").textContent = `Maze ${maze.id}`;
+      cardContainer.querySelector("#maze-size").textContent = `${getMazeSize(maze.maze_layout)}`;
+      cardContainer.querySelector("#maze-difficulty").textContent = '⭐'.repeat(maze.difficulty_level_id);
+      cardContainer.querySelector("#maze-time-record").textContent = `Best Time: ${"N/A"}`;
+      cardContainer.querySelector("#maze-step-record").textContent = `Best Steps Taken: ${"N/A"}`;
 
-            return cardContainer;
-        })
+      return cardContainer;
+    })
 }
