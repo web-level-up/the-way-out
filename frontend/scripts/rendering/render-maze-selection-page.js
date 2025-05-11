1255149d9cf525c6edda406ca89aa57e292169da
Ryan Christie <ryan.christie@bbd.co.za>
2025-05-11 20:30:16 +0200

    Integration of maze selection (commented out for now)

--- frontend/scripts/rendering/render-maze-selection-page.js
+++ frontend/scripts/rendering/render-maze-selection-page.js
@@ -1,19 +1,68 @@
-import {dummyMazes} from "../dummy-data.js";
-import {renderMazeCard} from "./render-maze-card.js";
-import {fetchContentPagePromise, getRenderBaseGamePagePromise} from "./helpers/helper-functions.js";
+import { dummyMazes } from "../dummy-data.js";
+import { getConfig } from "../config-loader.js";
+import { renderMazeCard } from "./render-maze-card.js";
+import {
+  fetchContentPagePromise,
+  getRenderBaseGamePagePromise,
+} from "./helpers/helper-functions.js";
 
 export function renderMazeSelectionPage() {
   getRenderBaseGamePagePromise().then(() => {
-    const contentContainer = document.getElementById('content');
+    const contentContainer = document.getElementById("content");
 
-    fetchContentPagePromise("maze-selection", contentContainer)
-      .then(() => {
-        const mazeContainer = contentContainer.querySelector("#mazes");
+    fetchContentPagePromise("maze-selection", contentContainer).then(() => {
+      const mazeContainer = contentContainer.querySelector("#mazes");
 
-        dummyMazes.forEach(maze => {
-          renderMazeCard(maze)
-            .then(card => mazeContainer.appendChild(card))
-        });
+      // const config = getConfig();
+      // return fetch(config.apiBaseUrl + "/api/mazes", {
+      //   method: "GET",
+      //   headers: {
+      //     "Content-Type": "application/json",
+      //     Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+      //   },
+      //   signal: AbortSignal.timeout(5000),
+      // })
+      //   .then(async (response) => {
+      //     if (!response.ok) {
+      //       const errorData = await response.json();
+      //       throw new HttpError(response.status, errorData.error);
+      //     }
+      //     return response.json();
+      //   })
+      //   .then((data) => {
+      //     data.forEach((maze) => {
+      //       renderMazeCard(maze).then((card) =>
+      //         mazeContainer.appendChild(card)
+      //       );
+      //     });
+      //   })
+      //   .catch((error) => {
+      //     if (error instanceof HttpError) {
+      //       if ((error.status = 401)) {
+      //         renderErrorPage(
+      //           "Your session has expired, you will need to login again",
+      //           renderLoginPage,
+      //           "return to login"
+      //         );
+      //       } else {
+      //         renderErrorPage(
+      //           error.message ?? "An unexpected error has occurred",
+      //           renderMazeSelectionPage,
+      //           "return to selection page"
+      //         );
+      //       }
+      //     } else {
+      //       renderErrorPage(
+      //         error ?? "An unexpected error has occurred",
+      //         renderMazeSelectionPage,
+      //         "return to selection page"
+      //       );
+      //     }
+      //   });
+
+      dummyMazes.forEach((maze) => {
+        renderMazeCard(maze).then((card) => mazeContainer.appendChild(card));
       });
+    });
   });
 }
