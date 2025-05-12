4bc06fd047ce8978483de09e7ec5cf4dc5a53605
Ryan Christie <ryan.christie@bbd.co.za>
2025-05-12 21:54:42 +0200

    Adding seconds to db col names

--- flyway/sql/V05.12.5__maze_times_cols.sql
+++ flyway/sql/V05.12.5__maze_times_cols.sql
@@ -1,15 +1,15 @@
 ALTER TABLE "difficulty_levels"
-ADD COLUMN "preview_time" integer NOT NULL DEFAULT 45,
-ADD COLUMN "escape_time" integer NOT NULL DEFAULT 90;
+ADD COLUMN "preview_time_seconds" integer NOT NULL DEFAULT 45,
+ADD COLUMN "escape_time_seconds" integer NOT NULL DEFAULT 90;
 
 UPDATE "difficulty_levels"
-SET "preview_time" = 60, "escape_time" = 120
+SET "preview_time_seconds" = 60, "escape_time_seconds" = 120
 WHERE "difficulty_level_name" = 'Easy';
 
 UPDATE "difficulty_levels"
-SET "preview_time" = 45, "escape_time" = 90
+SET "preview_time_seconds" = 45, "escape_time_seconds" = 90
 WHERE "difficulty_level_name" = 'Medium';
 
 UPDATE "difficulty_levels"
-SET "preview_time" = 30, "escape_time" = 60
+SET "preview_time_seconds" = 30, "escape_time_seconds" = 60
 WHERE "difficulty_level_name" = 'Hard';
