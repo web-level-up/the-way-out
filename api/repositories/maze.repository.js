d4d2f02bab5844bfdf2853c1934829290cdcdfcf
Temwa-BBD <temwa.nyirenda@vcontractor.co.za>
2025-05-13 01:51:25 +0200

    Added leaderboard page

--- api/repositories/maze.repository.js
+++ api/repositories/maze.repository.js
@@ -1,7 +1,17 @@
 import sql from "../config/db.js";
 
 export const getAllMazes = () =>
-  sql`SELECT m.id, m.maze_layout, m.difficulty_level_id as difficulty_id, d.difficulty_level_name as difficulty_name, m.starting_position, m.ending_position FROM mazes m INNER JOIN difficulty_levels d ON m.difficulty_level_id = d.id`;
+  sql`SELECT 
+        m.id, 
+        m.maze_layout, 
+        m.difficulty_level_id as difficulty_id, 
+        d.difficulty_level_name as difficulty_name, 
+        m.starting_position, 
+        m.ending_position 
+      FROM mazes m 
+        INNER JOIN difficulty_levels d 
+        ON m.difficulty_level_id = d.id
+      ORDER BY m.id ASC`;
 
 export const getMazeById = (id) =>
   sql`SELECT m.id, m.maze_layout, m.difficulty_level_id as difficulty_id, d.difficulty_level_name as difficulty_name, m.starting_position, m.ending_position FROM mazes m INNER JOIN difficulty_levels d ON m.difficulty_level_id = d.id WHERE m.id = ${id}`.then(
@@ -18,10 +28,29 @@ export const postCompletion = (mazeId, playerGoogleId, timeTaken, stepsTaken) =>
 
 export const getLeaderboard = (mazeId) =>
   sql`
+    SELECT u.username, first_completions.time_taken, first_completions.steps_taken
+    FROM (
+      SELECT mc_inner.*
+      FROM maze_completions mc_inner
+      WHERE mc_inner.maze_id = ${mazeId}
+        AND NOT EXISTS (
+          SELECT 1
+          FROM maze_completions mc_older
+          WHERE mc_older.user_id = mc_inner.user_id
+            AND mc_older.maze_id = mc_inner.maze_id
+            AND mc_older.id < mc_inner.id
+        )
+    ) AS first_completions
+      INNER JOIN users u ON first_completions.user_id = u.id
+    ORDER BY first_completions.time_taken ASC
+        LIMIT 10;
+  `;
+export const getUserMazeCompletions = (mazeId, userId) =>
+  sql`
     SELECT u.username, mc.time_taken, mc.steps_taken
     FROM maze_completions mc
-    INNER JOIN users u ON mc.user_id = u.id
-    WHERE maze_id = ${mazeId}
-    ORDER BY time_taken ASC
-    LIMIT 10
+      INNER JOIN users u ON mc.user_id = u.id
+    WHERE mc.maze_id = ${mazeId}
+        AND mc.user_id = ${userId}
+    ORDER BY mc.id ASC;
   `;
--- frontend/scripts/rendering/render-main-page.js
+++ frontend/scripts/rendering/render-main-page.js
@@ -1,6 +1,9 @@
-import { logout } from "../util.js";
+import {getDataFromUrl, logout} from "../util.js";
 import { renderMazeSelectionPage } from "./render-maze-selection-page.js";
 import { loadPage } from "./renderer.js";
+import {renderLeaderboardPage} from "./render-leaderboard-page.js";
+import {getConfig} from "../config-loader.js";
+import {HttpError} from "../custom-errors.js";
 
 export function renderMainPage() {
   return loadPage("views/main-page.html").then(() => {
@@ -8,9 +11,24 @@ export function renderMainPage() {
       .getElementById("maze-selection")
       .addEventListener("click", renderMazeSelectionPage);
     document
+      .getElementById("leaderboard")
+      .addEventListener("click", renderLeaderboardPage);
+    document
       .getElementById("logout")
       .addEventListener("click", logout);
-    document.getElementById("welcome").textContent = `Welcome ${localStorage.getItem("username")}! Do You Remember the Way Out?`
+    document
+      .getElementById("welcome").textContent =
+        `Welcome ${localStorage.getItem("username")}! Do You Remember the Way Out?`
+
+    retrieveAndStoreMazes();
   });
-  
+}
+
+function retrieveAndStoreMazes() {
+  getDataFromUrl("/api/mazes")
+    .then((mazes) => {
+      console.log('mazes', mazes);
+
+      localStorage.setItem('mazes', JSON.stringify(mazes));
+    });
 }
