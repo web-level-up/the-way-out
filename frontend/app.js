d8ac52e3ecb33523f96710b8d6a23ea135221226
Tshepo Ncube <Tshepo.Ncube@bbd.co.za>
2025-05-13 15:05:03 +0200

    sddss

--- frontend/app.js
+++ frontend/app.js
@@ -6,20 +6,19 @@ import { renderErrorPage } from "./scripts/rendering/render-error.js";
 import { renderMainPage } from "./scripts/rendering/render-main-page.js";
 import { renderMazeGame } from "./scripts/rendering/render-maze-game.js";
 import { renderMazeDetailsPage } from "./scripts/rendering/render-maze-details.js";
-// await loadConfig();
-// const loginState = await handleOAuthCallback();
-// switch (loginState) {
-//     renderMainPage();
-//     break;
-//   case "new":
-//     renderUsernamePage();
-//     break;
-//   case "failed":
-//     renderErrorPage("Login failed", renderLoginPage, "return to login");
-//     break;
-//   default:
-//     renderLoginPage();
-// }
-
-//renderMainPage();
-renderMazeGame();
+await loadConfig();
+const loginState = await handleOAuthCallback();
+switch (loginState) {
+  case "existing":
+    renderMainPage();
+    break;
+  case "new":
+    renderUsernamePage();
+    break;
+  case "failed":
+    renderErrorPage("Login failed", renderLoginPage, "return to login");
+    break;
+  default:
+    renderLoginPage();
+}
+// renderMainPage();
