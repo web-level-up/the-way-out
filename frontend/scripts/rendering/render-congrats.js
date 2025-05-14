611d28ee9b2c728ff359fc302580fd682331220c
SameerGhela <sameer.ghela@bbd.co.za>
2025-05-14 22:14:42 +0200

    Congrats Page Nav

--- frontend/scripts/rendering/render-congrats.js
+++ frontend/scripts/rendering/render-congrats.js
@@ -1,7 +1,19 @@
 import { loadPage } from "./renderer.js";
+import { renderMazeSelectionPage } from "./render-maze-selection-page.js";
+import { renderLeaderboardPage } from "./render-leaderboard-page.js";
 import { renderMainPage } from "./render-main-page.js";
 import { renderMazeGame } from "./render-maze-game.js";
 
 export function renderCongrats() {
-  loadPage("/views/game-congrats.html").then(() => {});
+  loadPage("./views/game-congrats.html").then(() => {
+    document
+          .getElementById("maze-selection")
+          .addEventListener("click", renderMazeSelectionPage);
+    document
+          .getElementById("leaderboard")
+          .addEventListener("click", renderLeaderboardPage);
+    document
+          .getElementById("main-page")
+          .addEventListener("click", renderMainPage);
+  });
 }
