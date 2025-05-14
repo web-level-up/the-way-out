a076180a6def417abb1628851bd2e8a4f086f534
SameerGhela <sameer.ghela@bbd.co.za>
2025-05-14 13:51:23 +0200

    Loss page navigation

--- frontend/scripts/rendering/render-loss.js
+++ frontend/scripts/rendering/render-loss.js
@@ -1,7 +1,16 @@
 import { loadPage } from "./renderer.js";
 import { renderMainPage } from "./render-main-page.js";
 import { renderMazeGame } from "./render-maze-game.js";
+import { renderMazeSelectionPage } from "./render-maze-selection-page.js";
+import { renderLeaderboardPage } from "./render-leaderboard-page.js";
 
 export function renderLoss() {
-  loadPage("./views/game-loss.html").then(() => {});
+  loadPage("./views/game-loss.html").then(() => {
+    document
+          .getElementById("maze-selection")
+          .addEventListener("click", renderMazeSelectionPage);
+    document
+          .getElementById("leaderboard")
+          .addEventListener("click", renderLeaderboardPage);
+  });
 }
